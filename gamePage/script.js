dbee546b55f85011aaa35af33f05346da00e239d
itu-itis24-uncuoglu22 <uncuoglu22@itu.edu.tr>
2025-01-19 16:55:19 +0300

    Add functionality for the game loop

--- gamePage/script.js
+++ gamePage/script.js
@@ -7,10 +7,10 @@ let playerLives = 3;
 let hearts = document.getElementById("hearts");
 
 let answer = ['C', 'L', 'O', 'U', 'D'];
+let cardImgs = ["card-c.svg", "card-l.svg", "card-o.svg", "card-u.svg", "card-d.svg"];
 let counter = 0;
 
 initialize()
-
 
 function initialize() {
     input.value = "";
@@ -34,3 +34,77 @@ function updateHearts() {
         hearts.appendChild(heartImg);
     }
 }
+
+function inputHandler(guess) {
+    if (typeof(guess) == "string"){
+        let trimmed_guess = guess.replace(/\s+/g, '');
+        trimmed_guess = trimmed_guess.toUpperCase();
+        console.log(`Input has been registered ${trimmed_guess}`);
+
+        if (trimmed_guess.length == 1) {
+            if (trimmed_guess === answer[counter]){
+                window.alert("YOUR GUESS IS CORRECT!");
+                counter += 1;
+
+                updateScore();
+                
+                if (counter == 5) {
+                    gameOverWin();
+                }
+            }
+            else {
+                window.alert("YOUR GUESS IS WRONG!");
+                playerLives -= 1;
+                updateHearts();
+
+                if (playerLives <= 0) {
+                    gameOverLose();
+                }
+            }
+        }
+        else if (trimmed_guess.length == 0) {
+            window.alert("ENTER A VALID INPUT: EMPTY INPUT")
+        }
+        else {
+            if (trimmed_guess === 'CLOUD') {
+                gameOverWin();
+            }
+            else {
+                playerLives = 0;
+                updateHearts();
+                gameOverLose();
+            }
+        }
+    }
+    if (counter != 0)
+    {
+        resetBtn.style.display = "block";
+    }
+}
+
+function updateScore() {
+    playerScore += 20;
+    scoreContainer.innerHTML = String(playerScore);
+}
+
+function updateScoreWin() {
+    playerScore = 100;
+    scoreContainer.innerHTML = String(playerScore);
+}
+
+function gameOverLose() {
+    window.alert("YOU'VE LOST...");
+    submitBtn.disabled = true;
+    submitBtn.style.backgroundColor = "gray";
+    resetBtn.style.display = "block";
+}
+
+function gameOverWin() {
+    updateScoreWin();
+    window.alert("YOU'VE WON!!!");
+    submitBtn.disabled = true;
+    submitBtn.style.backgroundColor = "gray";
+}
+
+submitBtn.addEventListener("click", () => inputHandler(input.value));
+resetBtn.addEventListener("click", () => initialize());
